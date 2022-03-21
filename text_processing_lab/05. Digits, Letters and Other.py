c1b89b6544e6672c8c671bafd752466dbc74dfc6
Stanislav Bozhimirov <bozhimirov@gmail.com>
2022-03-21 23:07:19 +0200

    solved

---
+++ text_processing_lab/05. Digits, Letters and Other.py
@@ -0,0 +1,15 @@
+text = input()
+digits = ""
+letters = ""
+symbols = ""
+
+for ch in text:
+    if ch.isdigit():
+        digits += ch
+    elif ch.isalpha():
+        letters += ch
+    else:
+        symbols += ch
+print(digits)
+print(letters)
+print(symbols)
