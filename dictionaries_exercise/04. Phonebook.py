430b6c094c9c88176d9274b179e975249d910345
Stanislav Bozhimirov <bozhimirov@gmail.com>
2022-07-24 22:46:07 +0300

    solved

---
+++ dictionaries_exercise/04. Phonebook.py
@@ -0,0 +1,27 @@
+def phone_book_information(number_of_lines, phone_book):
+    for x in range(number_of_lines):
+        name = input()
+        if name not in phone_book:
+            print(f"Contact {name} does not exist.")
+        else:
+            print(f"{name} -> {phone_book[name]}")
+
+    return True
+
+
+def phonebook():
+    phone_book = {}
+    condition = False
+
+    while True:
+        contact_information = input().split("-")
+
+        if contact_information[0].isdigit():
+            condition = phone_book_information(int(contact_information[0]),phone_book)
+        else:
+            phone_book[contact_information[0]] = contact_information[1]
+
+        if condition:
+            break
+
+phonebook()
