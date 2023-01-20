be3fe787dbda861272c249519ed4a0233c24ce1a
Stanislav Bozhimirov <bozhimirov@gmail.com>
2023-01-20 22:17:53 +0200

    solved

---
+++ list_basics_exercise/10. Bread Factory.py
@@ -0,0 +1,39 @@
+energy = 100
+coins = 100
+events = input().split('|')
+close_condition = False
+
+for event in events:
+    current_event_element = event.split('-')
+    command = current_event_element[0]
+    number = int(current_event_element[1])
+
+    if command == 'rest':
+        if energy >= 100:
+            energy = 100
+            print(f'You gained 0 energy.')
+        else:
+            energy += number
+            print(f'You gained {number} energy.')
+        print(f'Current energy: {energy}.')
+    elif command == 'order':
+        if energy >= 30:
+            print(f'You earned {number} coins.')
+            energy -= 30
+            coins += number
+        else:
+            energy += 50
+            print('You had to rest!')
+    else:
+        if coins >= number:
+            print(f'You bought {command}.')
+            coins -= number
+        else:
+            print(f'Closed! Cannot afford {command}.')
+            close_condition = True
+            break
+
+if not close_condition:
+    print(f'Day completed!')
+    print(f'Coins: {coins}')
+    print(f'Energy: {energy}')
